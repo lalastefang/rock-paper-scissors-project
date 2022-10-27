1c80aada2d2e80d84abc2c24300225aabb57f0e1
lalastefang <99011343+lalastefang@users.noreply.github.com>
2022-10-27 15:40:29 +1100

    Put the comments in!

--- brains.js
+++ brains.js
@@ -1,48 +1,65 @@
-function getComputerChoice(RPS) {
+function getComputerChoice() {
     // Returns a random integer from 0 to 3:
     let greeting = Math.floor(Math.random() * 3);
     //Randomly parse Rock, Paper or Scissors
     switch(greeting) {
         case 1:
-            RPS = "Rock";
+            return "Rock";
             break;
         case 2:
-            RPS = "Paper";
+            return "Paper";
             break;
         default:
-            RPS = "Scissors";
+            return "Scissors";
     }
+
 }
-getComputerChoice();
+
 
 function playRound(playerSelection, computerSelection){
+    
     //obtain the player choice, case insensitive e.g. rock, Rock, ROCK, roCK
-    let player = prompt("Play Rock Paper Scissors by typing an option");
-    playerSelection === player.toUpperCase;
-    computerSelection === getComputerChoice.toUpperCase;
+    playerSelection = prompt("Play Rock Paper Scissors by typing an option");
+    let player = playerSelection.toLowerCase();
+    
     //display message is displayed to the user 
         //If player and computer choice are the same, then show "It's a tie!"
-        if (playerSelection = computerSelection) {
-            return player = "It's a tie!";
-        }
-        //If player loses, then show "You Lose! ${computerSelection} beats ${playerSelection}"
+        let outcomeTieMessage = "You got a tie.";
+        let outcomeTie = (player == computerSelection.toLowerCase());
+
         //If player chooses Scissors and computer chooses Rock, player loses
         //If player chooses Rock and computer chooses Paper, player loses
         //If player chooses Paper and computer chooses Scissors, player loses
-        else if ((playerSelection == "ROCK" && computerSelection == "PAPER")
-            || (playerSelection == "SCISSORS" && computerSelection == "ROCK") 
-            || (playerSelection == "PAPER" && computerSelection == "SCISSORS")) {
-                return player = "You lost!";
-        }
-        //If player wins, then show "You Win! ${playerSelection} beats ${computerSelection}"
+        let outcomeLoseMessage = "You lost.";
+        let outcomeLose = (
+            (player == "rock" && computerSelection == "Paper") || 
+            (player == "paper" && computerSelection == "Scissors") || 
+            (player == "scissors" && computerSelection == "Rock")
+            );
+        
         //If player chooses Scissors and computer chooses Paper, player wins
         //If player chooses Rock and computer chooses Scissors, player wins
         //If player chooses Paper and computer chooses Rock, player wins
-        else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS")
-            || (playerSelection == "SCISSORS" && computerSelection == "PAPER")
-            || (playerSelection == "PAPER" && computerSelection == "ROCK")){
-                return player = "You won!";
-            }
+        let outcomeWinMessage = "You won!";
+        let outcomeWin = (
+            (player == "rock" && computerSelection == "Scissors") || 
+            (player == "paper" && computerSelection == "Rock") || 
+            (player == "scissors" && computerSelection == "Paper")
+            );
+
+         //If player wins, then show "You Win! ${playerSelection} beats ${computerSelection}"
+        if (outcomeWin) {
+            return outcomeWinMessage;
+        }
+
+        //If player loses, then show "You Lose! ${computerSelection} beats ${playerSelection}"
+        else if (outcomeLose) {
+            return outcomeLoseMessage;
+        }
+        //If player and computer choice are the same, then show "It's a tie!"
+        else if (outcomeTie) {
+            return outcomeTieMessage;
+        }
     
 }
 
